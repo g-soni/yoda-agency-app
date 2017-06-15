8d3c6d4f0f78153209d448ef5ccac542197d765f
Gaurav <gauravsoni.grep@gmail.com>
2017-06-15 11:41:01 +0530

    added required plugins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { UIRouterModule } from "@uirouter/angular";
+import { MaterialModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { AgencyComponent } from './agency/agency.component';
+
+let agencyState = { name: "agency", redirectTo: "/",  component: AgencyComponent }; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgencyComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    NoopAnimationsModule,
+    UIRouterModule.forRoot({ states: [ agencyState ], useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
